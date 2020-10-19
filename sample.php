afdc9c67ea36f3fd33c224e36213fe6317496289
Ryuto.Miyahira <gyakusyuyou.miya@gmail.com>
2020-10-19 11:54:40 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset "UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <boby>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果:無し</p>
+        <form action="sample.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじをひく!!</button>
+        </form>
+    </boby>
+</html>
